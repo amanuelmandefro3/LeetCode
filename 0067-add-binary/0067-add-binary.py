21e0ce5dba90ea61f9e8f95a1ee0b3a460d3f44e
theamano <106990287+amanuelmandefro3@users.noreply.github.com>
2024-05-01 07:44:19 +0300

    Time: 39 ms (44.58%), Space: 16.8 MB (11.38%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,4 +1,17 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        return bin(int(a,2)+int(b,2))[2:]
+        a = list(a)
+        b = list(b)
+        result = []
+        carry = 0
+        while a or b or carry:
+            if a:
+                carry += int(a.pop())
+            if b:
+                carry += int(b.pop())
+            result.append(str(carry%2))
+            carry //= 2
+        result = ''.join(result)
+       
+        return result[::-1]
         
