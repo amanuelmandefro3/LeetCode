d41c34801eaf93724c1d027802dd17afc59295d8
theamano <106990287+amanuelmandefro3@users.noreply.github.com>
2024-05-03 10:34:14 +0300

    Time: 764 ms (48.94%), Space: 18 MB (55.32%) - LeetHub

---
+++ 3189-find-champion-ii/3189-find-champion-ii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findChampion(self, n: int, edges: List[List[int]]) -> int:
+        indegree = defaultdict(int)
+        for u, v in edges:
+            indegree[v]+= 1
+        src = []
+        for i in range(n):
+            if not indegree[i]:
+                src.append(i)
+        if len(src) > 1:
+            return -1
+        return src[0]    
+        
