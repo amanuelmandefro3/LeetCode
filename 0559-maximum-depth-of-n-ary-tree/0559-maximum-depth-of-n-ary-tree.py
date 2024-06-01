274f55757bdab3a109e65d1b68c518ee003cc300
theamano <106990287+amanuelmandefro3@users.noreply.github.com>
2024-06-01 21:30:09 +0300

    Time: 42 ms (66.71%), Space: 17.9 MB (89.82%) - LeetHub

---
+++ 0559-maximum-depth-of-n-ary-tree/0559-maximum-depth-of-n-ary-tree.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        
+        ans = 0
+        queue = deque()
+        if root:
+            queue.append(root)
+        
+        while queue:
+            ans += 1
+            n = len(queue)
+            for _ in range(n):
+                curr = queue.popleft()
+                if curr.children:
+                    for ngb in curr.children:
+                        queue.append(ngb)
+        
+        return ans
