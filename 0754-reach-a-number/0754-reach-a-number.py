cd5771839a4a56024012420513435279ed913f0a
theamano <106990287+amanuelmandefro3@users.noreply.github.com>
2024-07-25 08:40:47 +0300

    Time: 71 ms (27.05%), Space: 16.6 MB (7.53%) - LeetHub

---
+++ 0754-reach-a-number/0754-reach-a-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reachNumber(self, target: int) -> int:
+        target = abs(target)
+        possible_moves, move = 0, 0
+      
+        while possible_moves < target or ( possible_moves - target) % 2 != 0:
+            move += 1
+            possible_moves += move
+
+
+        return move
+        
