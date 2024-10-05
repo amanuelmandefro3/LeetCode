3fc424d736cf8bf37e9f79ec0dad19c53d0d6176
theamano <106990287+amanuelmandefro3@users.noreply.github.com>
2024-10-05 20:25:08 +0300

    Time: 827 ms (5.08%), Space: 39.4 MB (6.34%) - LeetHub

---
+++ 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.py
@@ -0,0 +1,12 @@
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        mx = max(nums)
+        ans, pos_dict = 1, defaultdict(int)
+        if nums[0] == mx: pos_dict[0] = 1
+        for i in range(1, len(nums)):
+            if nums[i] == mx:
+                pos_dict[i] +=( pos_dict[i-1] + 1)
+                ans = max(pos_dict[i], ans)
+
+        return ans        
+        
