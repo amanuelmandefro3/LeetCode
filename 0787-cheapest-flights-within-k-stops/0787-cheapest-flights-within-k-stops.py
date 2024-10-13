909b3984653e9bb04731918a9816b13dce8903b4
theamano <106990287+amanuelmandefro3@users.noreply.github.com>
2024-10-14 00:14:32 +0300

    Time: 176 ms (27.54%), Space: 17.7 MB (81.9%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -3,12 +3,10 @@ class Solution:
         dist = [float('inf')] * n
         dist[src] = 0
 
-       
         for _ in range(k + 1):
             curr = dist[:]
             for u, v, w in flights:
-                if dist[u] != float('inf'): 
-                    curr[v] = min(curr[v], dist[u] + w)
+                curr[v] = min(curr[v], dist[u] + w)
             dist = curr[:]
 
         return dist[dst] if dist[dst] != float('inf') else -1
